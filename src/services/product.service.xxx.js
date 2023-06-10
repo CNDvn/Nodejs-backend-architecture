ddee1440eda03bb58c17302b04116ef48475481f
CNDvn <banhduchieucode@gmail.com>
2023-06-11 05:41:12 +0700

    section 19: Inventories Model vs Services
    Inventories Model vs Services

--- src/services/product.service.xxx.js
+++ src/services/product.service.xxx.js
@@ -9,6 +9,7 @@ const {
 const { BadRequestError } = require("../core/error.response");
 const { findAllDraftsForShop, publishProductByShop, findAllPublishForShop, unPublishProductByShop, searchProductByUser, findAllProducts, findProduct, updateProductById } = require("../models/repositories/product.repo");
 const { removeUndefinedObject, updateNestedObjectParser } = require("../utils");
+const { insertInventory } = require("../models/repositories/inventory.repo");
 
 // define Factory class to create product
 class ProductFactory {
@@ -95,7 +96,16 @@ class Product {
 
   // create new product
   async createProduct(product_id) {
-    return await product.create({ ...this, _id: product_id });
+    const newProduct = await product.create({ ...this, _id: product_id });
+    if(newProduct){
+      // add product_stock in inventory collection
+      await insertInventory({
+        productId: newProduct._id,
+        shopId: this.product_shop,
+        stock: this.product_quantity
+      })
+    }
+    return newProduct
   }
 
   // update Product
